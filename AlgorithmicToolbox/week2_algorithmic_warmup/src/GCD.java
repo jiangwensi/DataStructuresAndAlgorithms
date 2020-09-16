e7d6d0bc3089f62973737b669076b144208c63e1
JIANG WENSI <jiangwensi@hotmail.com>
2020-09-16 14:56:48 +0800

    add GCD

--- AlgorithmicToolbox/week2_algorithmic_warmup/src/GCD.java
+++ AlgorithmicToolbox/week2_algorithmic_warmup/src/GCD.java
@@ -14,11 +14,24 @@ public class GCD {
     return current_gcd;
   }
 
+  private static int gcd_fast(int a, int b){
+    if(a%b==0){
+      return b;
+    } else if(b%a==0){
+      return a;
+    }
+    if(a>b){
+      return gcd_fast(a%b,b);
+    } else {
+      return gcd_fast(a,b%a);
+    }
+  }
+
   public static void main(String args[]) {
     Scanner scanner = new Scanner(System.in);
     int a = scanner.nextInt();
     int b = scanner.nextInt();
 
-    System.out.println(gcd_naive(a, b));
+    System.out.println(gcd_fast(a, b));
   }
 }
