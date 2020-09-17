bbea4e882cab74fe1c232d2b1b5c5b49061a211b
JIANG WENSI <jiangwensi@hotmail.com>
2020-09-17 11:13:02 +0800

    update

--- AlgorithmicToolbox/week3_greedy_algorithms/src/CarFueling.java
+++ AlgorithmicToolbox/week3_greedy_algorithms/src/CarFueling.java
@@ -2,9 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class CarFueling {
-    static int computeMinRefills(int dist, int tank, int[] stops) {
-        return -1;
-    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -18,4 +15,56 @@ public class CarFueling {
 
         System.out.println(computeMinRefills(dist, tank, stops));
     }
+
+    static int computeMinRefills(int dist, int tank, int[] stops) {
+        int count = 0;
+        int current = 0;
+        int n = stops.length;
+        int[] allStops = new int[n + 2];
+        allStops[0]=0;
+        for (int i = 1; i <= n; i++) {
+            allStops[i] = stops[i-1];
+        }
+        allStops[n+1]=dist;
+
+        int nextRefill = 0;
+        while(current<=n){
+            while(nextRefill+1 <=n+1 && allStops[nextRefill+1]-allStops[current]<=tank ){
+                ++nextRefill;
+            }
+            if(nextRefill == current) {
+                return -1;
+            } else {
+                if(nextRefill!=n+1){
+                    count++;
+                }
+                current = nextRefill;
+            }
+        }
+
+        return count;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
