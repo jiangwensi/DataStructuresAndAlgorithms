801e79afefebf5e03cdaf299c21d89c2415560d5
JIANG WENSI <jiangwensi@hotmail.com>
2020-09-30 15:40:08 +0800

    update

--- AlgorithmsOnStrings/Programming-Assignment-2/src/InverseBWT.java
+++ AlgorithmsOnStrings/Programming-Assignment-2/src/InverseBWT.java
@@ -1,7 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
 public class InverseBWT {
     class FastScanner {
@@ -24,99 +27,107 @@ public class InverseBWT {
     }
 
     String inverseBWT(String bwt) {
+
+        //build matrixStart for first column in bwt matrix
+        //col1 is char, col2 is order
+        int[][] matrixStart = new int[bwt.length()][];
+        for (int i = 0; i < matrixStart.length; i++) {
+            matrixStart[i] = new int[2];
+        }
+
+        char[] chars = bwt.toCharArray();
+        char[] charsSorted = sortChars(chars);
+        int[] count = new int[5];
+        for (int i = 0; i < matrixStart.length; i++) {
+            matrixStart[i][0]=charToInteger(charsSorted[i]);
+            matrixStart[i][1]=count[charToInteger(charsSorted[i])];
+        }
+
+        //build matrixStart for last column in bwt matrix
+        //col1 is char, col2 is order
+        int[][] matrixEnd = new int[bwt.length()][];
+        for (int i = 0; i < matrixEnd.length; i++) {
+            matrixEnd[i] = new int[2];
+        }
+
+        count = new int[5];
+        for (int i = 0; i < matrixEnd.length; i++) {
+            matrixEnd[i][0]=charToInteger(chars[i]);
+            matrixEnd[i][1]=count[charToInteger(chars[i])];
+        }
+
+        //build pos arr for matrixStart, col1 is char, col2 is order, value is pos in column 1
+        ArrayList<Integer>[] posInMatrixStart = new ArrayList[5];//5 char
+        for(int i = 0; i < posInMatrixStart.length; i ++){
+            posInMatrixStart[i]=new ArrayList<>();//order
+        }
+
+        for(int i = 0; i < matrixStart.length; i ++){
+            posInMatrixStart[matrixStart[i][0]].add(i);
+        }
+
+        //build order arr for matrixEnd, what's the char order in a position
+        int[] orderInMatrixEnd = new int[bwt.length()];//index is position, value is the char order
+        count = new int[5];
+        for(int i = 0; i < chars.length;i++){
+            orderInMatrixEnd[i]=++count[charToInteger(chars[i])];
+        }
+
         StringBuilder result = new StringBuilder();
 
-        // write your code here
-        Character[] chars = bwt.chars().mapToObj(c -> (char) c).toArray(Character[]::new);
-        ArrayList<Character>[] matrix = new ArrayList[bwt.length()];
-        for (int i = 0; i < matrix.length; i++) {
-            matrix[i] = new ArrayList<>();
+        int index = 0;
+        while(result.length()<bwt.length()){
+            result.append(integerToChar(matrixStart[index][0]));
+            int match = matrixEnd[index][0];
+            index = posInMatrixStart[match].get(orderInMatrixEnd[index]-1);
         }
-
-        Character[] charsSorted = new Character[matrix.length];
-        for(int i = 0; i<charsSorted.length; i++){
-            charsSorted[i]=chars[i];
-        }
-
-        sortChars(charsSorted);
-
-        for (int i = 0; i < matrix.length; i++) {
-            matrix[0].add(charsSorted[i]);
-            matrix[matrix.length-1].add(chars[i]);
-        }
-//        ArrayList<Character>[] helper=null;
-        for (int endIndex = 1; endIndex < matrix.length - 1; endIndex++) {
-            ArrayList<Character>[] helper = new ArrayList[matrix.length];
-            for (int i = 0; i < matrix.length; i++) {
-                helper[i] = new ArrayList<Character>();
-            }
-            for (int i = 0; i < matrix.length; i++) {
-                helper[i].add(chars[i]);
-            }
-            for (int i = 0; i < matrix.length; i++) {
-                for (int j = 0; j < endIndex; j++) {
-                    helper[i].add(matrix[j].get(i));
-                }
-            }
-            sortArrayList(helper);
-            for(int i = 0; i < matrix.length; i++){
-                matrix[endIndex].add(helper[i].get(endIndex));
-            }
-            int i = 0;
-        }
-
-        for(int i = 0;i<matrix.length-1; i++){
-            result.append(matrix[i+1].get(0));
-        }
-        result.append("$");
-        return result.toString();
+        return result.reverse().toString();
     }
 
-    private void sortArrayList(ArrayList<Character>[] helper) {
-        Arrays.sort(helper, new Comparator<ArrayList<Character>>() {
-            @Override
-            public int compare(ArrayList<Character> o1, ArrayList<Character> o2) {
-                //find the index of first different character
-                int index = -1;
-                for (int i = 0; i < o1.size(); i++) {
-                    if (o1.get(i) != o2.get(i)) {
-                        index = i;
-                        break;
-                    }
-                }
+    private char[] sortChars(char[] chars) {
 
-                if (index == -1) {
-                    //equal if all characters are same
-                    return 0;
-                } else {
-//                    if(o1.get(index).equals('$')){
-//                        return -1;
-//                    } else if(o2.get(index).equals('$')){
-//                        return 1;
-//                    } else {
-                        return o1.get(index).compareTo(o2.get(index));
-//                    }
-                }
+        int[] count = new int[5];
+        for (int i = 0; i < chars.length; i++) {
+            count[charToInteger(chars[i])]++;
+        }
+
+        char[] sorted = new char[chars.length];
+        int index = 0;
+        for (int i = 0; i < count.length; i++) {
+            for (int j = 0; j < count[i]; j++) {
+                sorted[index] = integerToChar(i);
+                index++;
             }
-        });
+        }
+        return sorted;
     }
 
-    private void sortChars(Character[] charsSorted) {
-        Arrays.sort(charsSorted, new Comparator<Character>() {
+    private int charToInteger(char value) {
+        if (value == '$') {
+            return 0;
+        } else if (value == 'A') {
+            return 1;
+        } else if (value == 'C') {
+            return 2;
+        } else if (value == 'G') {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
 
-            @Override
-            public int compare(Character o1, Character o2) {
-//                if (o1.equals(o2)) {
-//                    return 0;
-//                } else if (o1.equals("$")) {
-//                    return -1;
-//                } else if (o2.equals("$")) {
-//                    return 1;
-//                } else {
-                    return o1.compareTo(o2);
-//                }
-            }
-        });
+    private char integerToChar(int value) {
+        if (value == 0) {
+            return '$';
+        } else if (value == 1) {
+            return 'A';
+        } else if (value == 2) {
+            return 'C';
+        } else if (value == 3) {
+            return 'G';
+        } else {
+            return 'T';
+        }
     }
 
     static public void main(String[] args) throws IOException {
