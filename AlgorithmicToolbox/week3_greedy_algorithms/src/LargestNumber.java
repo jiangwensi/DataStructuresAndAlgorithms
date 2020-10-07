cd0875fe558ff8793b3d6b7e9c3fa83148e8bad6
JIANG WENSI <jiangwensi@hotmail.com>
2020-10-07 11:35:40 +0800

    update

---
+++ AlgorithmicToolbox/week3_greedy_algorithms/src/LargestNumber.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class LargestNumber {
+    private static String largestNumber(String[] a) {
+        //write your code here
+        String result = "";
+//        List<String> digitsStr = new LinkedList(Arrays.asList(a));
+        List<String> digits = new LinkedList(Arrays.asList(a));
+//        List<Integer> digits = digitsStr.stream().map(d->Integer.parseInt(d)).collect(Collectors.toCollection(LinkedList::new));
+        while (digits.size() > 0) {
+            String maxDigit = digits.get(0);
+            int maxIndex = 0;
+            for (int i = 0; i < digits.size(); i++) {
+                if (isGreaterOrEqual(digits.get(i) , maxDigit)) {
+                    maxDigit = digits.get(i);
+                    maxIndex = i;
+                }
+            }
+
+            result += maxDigit;
+            digits.remove(maxIndex);
+        }
+        return result;
+    }
+
+    private static boolean isGreaterOrEqual(String digit, String max) {
+        int len = min(digit.length(), max.length());
+        for(int i = 0; i < len; i ++){
+            if(digit.length()<=i){
+                return false;
+            }
+            if(max.length()<=i){
+                return true;
+            }
+            if(digit.charAt(i)>=max.charAt(i)){
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static int min(int a, int b) {
+        if(a<b){
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String[] a = new String[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.next();
+        }
+        System.out.println(largestNumber(a));
+    }
+}
+
