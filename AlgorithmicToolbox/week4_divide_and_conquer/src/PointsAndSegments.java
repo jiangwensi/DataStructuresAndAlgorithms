aa175ea0a469e700cdf57337676d89d889ddfba1
JIANG WENSI <jiangwensi@hotmail.com>
2020-10-08 16:04:20 +0800

    update

--- AlgorithmicToolbox/week4_divide_and_conquer/src/PointsAndSegments.java
+++ AlgorithmicToolbox/week4_divide_and_conquer/src/PointsAndSegments.java
@@ -10,32 +10,105 @@ public class PointsAndSegments {
         for (int i = 0; i < points.length; i++) {
             int l = -1;//number of segments having left end point to the left of x or equal to x
             int r = -1;//number of segments having right end point to the right of x of equal to x
-            Arrays.sort(starts);
-            for (int s = 0; s < starts.length; s++) {
-                if(starts[s]>points[i]){
-                    l=s-1+1;
-                    break;
-                }
-            }
-            if(l==-1){
-                l=starts.length;
-            }
 
-            Arrays.sort(ends);
-            for (int e = ends.length-1; e >=0; e--) {
-                if(ends[e]<points[i]){
-                    r=ends.length-e-1;
-                    break;
-                }
-            }
-            if(r==-1){
-                r= ends.length;
-            }
-            cnt[i]=l+r-starts.length;
+            int[] startSorted = mergeSortIntArr(starts, 0, starts.length, false);
+            l = bs(startSorted, 0, startSorted.length-1, points[i])+1;
 
+//            for (int s = 0; s < startSorted.length; s++) {
+//                if (startSorted[s] > points[i]) {
+//                    l = s - 1 + 1;
+//                    break;
+//                }
+//            }
+//
+//            if (l == -1) {
+//                l = startSorted.length;
+//            }
+
+            int[] endSorted = mergeSortIntArr(ends, 0, ends.length, false);
+            r = endSorted.length - bs(endSorted, 0, endSorted.length-1, points[i])-1;
+//            for (int e = 0; e < ends.length; e++) {
+//                if (endSorted[e] < points[i]) {
+//                    r = e - 1 + 1;
+//                    break;
+//                }
+//            }
+
+//            if (r == -1) {
+//                r = ends.length;
+//            }
+            cnt[i] = l + r - starts.length;
         }
 
         return cnt;
+    }
+
+    private static int bs(int[] sorted, int left, int right, int point) {
+
+        if (right < left) {
+            return left - 1;
+        }
+        int mid = (left + right) / 2;
+
+        if (point < sorted[mid]) {
+            return bs(sorted, left, mid - 1, point);
+        } else if (point > sorted[mid]) {
+            return bs(sorted, mid + 1, right, point);
+        } else {
+            return mid;
+        }
+    }
+
+    private static int[] mergeSortIntArr(int[] arr, int left, int right, boolean reverse) {
+        if (right - left == 1) {
+            int[] sorted = new int[1];
+            sorted[0] = arr[left];
+            return sorted;
+        }
+        int mid = (left + right) / 2;
+        int[] leftSorted = mergeSortIntArr(arr, left, mid, reverse);
+        int[] rightSorted = mergeSortIntArr(arr, mid, right, reverse);
+        return merge(leftSorted, rightSorted, reverse);
+    }
+
+    private static int[] merge(int[] leftSorted, int[] rightSorted, boolean reverse) {
+        int left = 0;
+        int right = 0;
+        int index = 0;
+        int[] sorted = new int[leftSorted.length + rightSorted.length];
+        while (left < leftSorted.length && right < rightSorted.length) {
+            if (leftSorted[left] <= rightSorted[right]) {
+                if (reverse) {
+                    sorted[index] = rightSorted[right];
+                    right++;
+                } else {
+                    sorted[index] = leftSorted[left];
+                    left++;
+                }
+            } else {
+                if (reverse) {
+                    sorted[index] = leftSorted[left];
+                    left++;
+                } else {
+                    sorted[index] = rightSorted[right];
+                    right++;
+                }
+            }
+            index++;
+        }
+        if (left < leftSorted.length) {
+            for (int i = left; i < leftSorted.length; i++) {
+                sorted[index] = leftSorted[i];
+                index++;
+            }
+        }
+        if (right < rightSorted.length) {
+            for (int i = right; i < rightSorted.length; i++) {
+                sorted[index] = rightSorted[i];
+                index++;
+            }
+        }
+        return sorted;
     }
 
 
