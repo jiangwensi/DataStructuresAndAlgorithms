c14c827820e2d31bc57d76e529e6200b0639a4fe
JIANG WENSI <jiangwensi@hotmail.com>
2020-10-11 20:29:56 +0800

    update

--- DataStructures/week1_basic_data_structures/src/StackWithMax.java
+++ DataStructures/week1_basic_data_structures/src/StackWithMax.java
@@ -1,5 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
 public class StackWithMax {
     class FastScanner {
@@ -15,15 +17,19 @@ public class StackWithMax {
                 tok = new StringTokenizer(in.readLine());
             return tok.nextToken();
         }
+
         int nextInt() throws IOException {
             return Integer.parseInt(next());
         }
     }
 
     public void solve() throws IOException {
+//        stressTest();
+//
         FastScanner scanner = new FastScanner();
         int queries = scanner.nextInt();
-        Stack<Integer> stack = new Stack<Integer>();
+        MyStack stack = new MyStack();
+        List maxList = new LinkedList<>();
 
         for (int qi = 0; qi < queries; ++qi) {
             String operation = scanner.next();
@@ -33,12 +39,80 @@ public class StackWithMax {
             } else if ("pop".equals(operation)) {
                 stack.pop();
             } else if ("max".equals(operation)) {
-                System.out.println(Collections.max(stack));
+                maxList.add(stack.max());
+            }
+        }
+        maxList.forEach(System.out::println);
+    }
+
+    private void stressTest() {
+        int size = 100000;
+        System.out.println(size);
+        MyStack stack0 = new MyStack();
+        for (int i = 0; i < size; i ++) {
+            String[] words = {
+                    "push", "push", "push", "max", "max", "pop", "max"
+            };
+            String word = words[new Random().nextInt(7)];
+            System.out.print(word+" ");
+            if (word.equals("push")) {
+                int value = new Random().nextInt(100000);
+                System.out.println(value);
+                stack0.push(value);
+            } else if (word.equals("pop")) {
+                System.out.println();
+                stack0.pop();
+            } else {
+                System.out.println(stack0.max());
             }
         }
     }
 
     static public void main(String[] args) throws IOException {
         new StackWithMax().solve();
+    }
+
+    private class MyStack {
+        MyNode head = new MyNode();
+        MyNode maxHead = new MyNode();
+
+        public void push(Integer value) {
+
+            if (maxHead.next == null) {
+                maxHead.next = new MyNode(value, maxHead.next);
+            } else {
+                if (maxHead.next.value <= value) {
+                    maxHead.next = new MyNode(value, maxHead.next);
+                }
+            }
+
+            head.next = new MyNode(value, head.next);
+        }
+
+        public void pop() {
+
+            if (maxHead.next.value == head.next.value) {
+                maxHead.next = maxHead.next.next;
+            }
+            head.next = head.next.next;
+        }
+
+        public int max() {
+            return maxHead.next.value;
+        }
+    }
+
+    private class MyNode {
+
+        MyNode next;
+        int value;
+
+        public MyNode() {
+        }
+
+        public MyNode(int value, MyNode next) {
+            this.next = next;
+            this.value = value;
+        }
     }
 }
--- DataStructures/week1_basic_data_structures/src/process_packages.java
+++ DataStructures/week1_basic_data_structures/src/process_packages.java
@@ -1,6 +1,7 @@
+package src;
+
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Scanner;
 
 class Request {
